b95527ba375cf68810a391aeebe9ec6677b8dbb6
Michallesss <mwiemszra@gmail.com>
2024-08-04 13:02:43 +0200

    💡 Add todos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+// TODO:
+// 1. Add rate limiting in this and other modules (https://docs.nestjs.com/security/rate-limiting)
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
